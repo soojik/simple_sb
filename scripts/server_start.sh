ccf1714df1ff5e546e239747e2098326524db1a6
pcmin929 <120066709+pcmin929@users.noreply.github.com>
2023-02-07 06:27:24 +0900

    Update server_start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -3,4 +3,5 @@
 #sudo /home/ec2-user/tomcat/bin/./startup.sh
 sudo chmod 755 /home/ec2-user/springbootApp.jar
 slepp 1
-sudo nohup java -jar /home/ec2-user/springbootApp.jar &
+sudo nohup nohup java -jar /home/ec2-user/springbootApp.jar 1> /dev/null 2>&1 &
+echo $! > pid.file
